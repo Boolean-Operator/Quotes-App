87e60eba3b68cc1038ff1fb52aefec7e01af74b3
Mark Graybill <marktgraybill@gmail.com>
2022-07-01 22:51:12 -0400

    Improve 404 - Error Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
+import hal from '../assets/hal.png';
 import classes from './NotFound.module.css';
+
 const NotFound = () => {
   return (
-    <div id="main">
+    <div id="main" className="centered hal">
+      <div>
+        <p>Error 404</p>
+        <img src={hal} alt="HAL 9000 error" />
+        <h1>I'm sorry Dave.</h1>
+      </div>
       <div className={classes.fof}>
-        <h1>Error 404</h1>
+        <h1>I'm afraid I can't do that.</h1>
       </div>
     </div>
   );
